3a21ddf28175f870d3e5a30b22064b95789dcbce
孔昀晖 <1032316751@qq.com>
2017-08-10 16:38:19 +0800

    cordova调用native - camera 1、添加插件：cordova plugin add cordova-plugin-camera 2、使用插件提供的全局变量进行camera操作：navigator.camera

--- vux-tutorial/src/components/Home.vue
+++ vux-tutorial/src/components/Home.vue
@@ -7,35 +7,79 @@
     <group title="cell demo">
       <cell title="VUX" value="go" link="/second"></cell>
     </group>
+    <card>
+      <x-img slot="header" :src="imgUrl" class="ximg-demo" container="#vux_view_box_body"></x-img>
+      <div slot="content">
+        <x-button @click.native="takePhoto">拍照</x-button>
+      </div>
+    </card>
+
   </div>
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+  import {Group, Cell, Card, XImg, XButton} from 'vux'
 
-export default {
-  components: {
-    Group,
-    Cell
-  },
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+  export default {
+    components: {
+      Group,
+      Cell,
+      Card,
+      XImg,
+      XButton
+    },
+    data () {
+      return {
+        // note: changing this line won't causes changes
+        // with hot-reload because the reloaded component
+        // preserves its current state and we are modifying
+        // its initial state.
+        msg: 'Hello World!',
+        imgUrl: 'https://o5omsejde.qnssl.com/demo/test1.jpg'
+      }
+    },
+    methods: {
+      takePhoto () {
+        if (!navigator.camera) {
+          window.alert('Camera API not supported !')
+          return
+        }
+        let options = {
+          quality: 50,
+          destinationType: navigator.camera.DestinationType.FILE_URI,
+          sourceType: navigator.camera.PictureSourceType.CAMERA,
+          encodingType: navigator.camera.EncodingType.JPEG,
+          mediaType: navigator.camera.MediaType.PICTURE,
+          allowEdit: true,
+          correctOrientation: true  // Corrects Android orientation quirks
+        }
+        let successCallback = function (imageURI) {
+          document.getElementsByClassName('ximg-demo')[0].src = imageURI
+        }
+        let errorCallback = function (message) {
+          window.alert('error:' + message)
+        }
+        navigator.camera.getPicture(successCallback, errorCallback, options)
+      }
     }
   }
-}
 </script>
 
-<style>
-.vux-demo {
-  text-align: center;
-}
-.logo {
-  width: 100px;
-  height: 100px
-}
+<style scoped lang="less">
+  @import '~vux/src/styles/1px.less';
+
+  .vux-demo {
+    text-align: center;
+  }
+
+  .logo {
+    width: 100px;
+    height: 100px
+  }
+
+  .ximg-demo {
+    width: 100%;
+    height: 400px;
+    display: block;
+  }
 </style>
